48c58f1194538660533ab0f1eb92923baa8bd42d
Merve Baykara <merve.baykara@veniture.com.tr>
2022-05-20 22:34:44 +0300

    added marker

---
+++ pages/map/MapPoints.js
@@ -0,0 +1,5 @@
+export const mapstaticpoints = [
+  { key: 1, title: "test1", latitude: 49.986111, longitude: 20.061667 },
+  /*  { key: 2, title: "test2", latitude: 50.193139, longitude: 20.288717 },
+    { key: 3, title: "test3", latitude: 49.740278, longitude: 19.588611 }, */
+];
---
+++ pages/map/MarkerMapScreen.js
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+import { View, StyleSheet, Image } from "react-native";
+import MapView, { Marker } from "react-native-maps";
+import mapstaticpoints from "./MapPoints";
+
+const MarkerMapScreen = () => {
+  const [mapRegion, setmapRegion] = useState({
+    latitude: 37.78825,
+    longitude: -122.4324,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+  });
+
+  return (
+    <View style={styles.container}>
+      <MapView
+        style={{ alignSelf: "stretch", height: "100%" }}
+        region={{
+          latitude: 40.758927,
+          longitude: -73.984981,
+          latitudeDelta: 0.1,
+          longitudeDelta: 0.1,
+        }}
+      >
+        {/*  <Marker coordinate={mapRegion} title="Marker" /> */}
+        {/*  {mapstaticpoints &&
+            mapstaticpoints.map((place, i) => (
+              <Marker key={i} title="Marker" coordinate={mapRegion} />
+            ))} */}
+        {mapstaticpoints &&
+          mapstaticpoints.map((marker, index) => (
+            <Marke
+              r
+              key={index}
+              coordinate={{
+                latitude: 40.758927,
+                longitude: -73.984981,
+              }}
+              title={marker.title}
+            />
+          ))}
+      </MapView>
+    </View>
+  );
+};
+
+export default MarkerMapScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
