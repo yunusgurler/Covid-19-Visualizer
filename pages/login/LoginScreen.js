1dfd2be886dc301435cb680053b38e846efb7cd2
Merve Baykara <merve.baykara@veniture.com.tr>
2022-05-23 20:10:12 +0300

    fixed error in login

--- pages/login/LoginScreen.js
+++ pages/login/LoginScreen.js
@@ -1,63 +1,42 @@
 import { useNavigation } from "@react-navigation/core";
-import React, { useEffect, useState } from 'react'
-import {
-  KeyboardAvoidingView,
-  StyleSheet,
-  Text,
-  TextInput,
-  TouchableOpacity,
-  View,
-  Image,
-} from "react-native";
-import {styles} from "./LoginScreenStyle";
-import { initializeApp } from 'firebase/app';
-import {
-  getAuth,
-} from 'firebase/auth';
-import { firebaseConfig } from "../../firebase";
+import React, { useEffect, useState } from "react";
+import { Text, TextInput, TouchableOpacity, View, Image } from "react-native";
+import { styles } from "./LoginScreenStyle";
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 import { signInWithEmailAndPassword } from "firebase/auth";
 
 const LoginScreen = () => {
-  const [email,setEmail] = useState("");
-  const [password,setPassword] = useState("");
-  const [errorMessage,setErrorMessage] = useState(false);
-
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState(false);
 
   const navigation = useNavigation();
-  initializeApp(firebaseConfig);
 
   const auth = getAuth();
 
-
   const handleLogin = async () => {
-   
-   try {
-        if (email !== '' && password !== '') {
-          await signInWithEmailAndPassword(auth,email, password)
+    try {
+      if (email !== "" && password !== "") {
+        await signInWithEmailAndPassword(auth, email, password)
           .then((userCredential) => {
             setErrorMessage(false);
             const user = userCredential.user;
             const accessToken = user?.accessToken;
-            if(accessToken && accessToken != "")  {
-              navigation.replace("Home");  
+            if (accessToken && accessToken != "") {
+              navigation.replace("Home");
             }
-         
           })
           .catch((error) => {
             const errorMessage = error.message;
             setErrorMessage(true);
           });
-         
-        }
-        else {
-          setErrorMessage(true)
-        }
-      } catch (error) {
-        throw error;
-     
-    };
-  
-   
+      } else {
+        setErrorMessage(true);
+      }
+    } catch (error) {
+      throw error;
+    }
   };
 
   const passToSignup = () => {
@@ -67,30 +46,29 @@ const LoginScreen = () => {
   return (
     <View style={styles.container} behavior="padding">
       <View style={styles.inputContainer}>
-      <Image 
-      style={styles.logo} 
-      source={require("../../assets/logo.png")}
-      />
+        <Image style={styles.logo} source={require("../../assets/logo.png")} />
         <TextInput
           placeholder="Email"
           value={email}
-          onChangeText={text => setEmail(text)}
+          onChangeText={(text) => setEmail(text)}
           style={styles.input}
         />
         <TextInput
           placeholder="Password"
           value={password}
-          onChangeText={text => setPassword(text)}
+          onChangeText={(text) => setPassword(text)}
           style={styles.input}
           secureTextEntry
         />
       </View>
 
-      {errorMessage && <Text style={{marginTop:20, color:"red"}}>Please provide correct credentials</Text>}
+      {errorMessage && (
+        <Text style={{ marginTop: 20, color: "red" }}>
+          Please provide correct credentials
+        </Text>
+      )}
 
       <View style={styles.buttonContainer}>
-        
-      
         <TouchableOpacity onPress={handleLogin} style={styles.loginButton}>
           <Text style={styles.buttonText}>Login</Text>
         </TouchableOpacity>
@@ -107,5 +85,3 @@ const LoginScreen = () => {
 };
 
 export default LoginScreen;
-
-
