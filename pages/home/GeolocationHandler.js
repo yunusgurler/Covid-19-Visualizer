91cbd440d71239566d4cf562ebcb053eb232c083
Merve Baykara <merve.baykara@veniture.com.tr>
2022-05-31 21:48:47 +0300

    added geolocation

---
+++ pages/home/GeolocationHandler.js
@@ -0,0 +1,69 @@
+import { StyleSheet, Text, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import * as Location from "expo-location";
+
+const GeolocationHandler = () => {
+  const [locationServiceEnabled, setLocationServiceEnabled] = useState(false);
+  const [displayCurrentAddress, setDisplayCurrentAddress] = useState(
+    "Wait, we are fetching you location..."
+  );
+
+  useEffect(() => {
+    CheckIfLocationEnabled();
+    GetCurrentLocation();
+  }, []);
+
+  const CheckIfLocationEnabled = async () => {
+    let enabled = await Location.hasServicesEnabledAsync();
+
+    if (!enabled) {
+      Alert.alert(
+        "Location Service not enabled",
+        "Please enable your location services to continue",
+        [{ text: "OK" }],
+        { cancelable: false }
+      );
+    } else {
+      setLocationServiceEnabled(enabled);
+    }
+  };
+
+  const GetCurrentLocation = async () => {
+    let { status } = await Location.requestPermissionsAsync();
+
+    if (status !== "granted") {
+      Alert.alert(
+        "Permission not granted",
+        "Allow the app to use location service.",
+        [{ text: "OK" }],
+        { cancelable: false }
+      );
+    }
+
+    let { coords } = await Location.getCurrentPositionAsync();
+
+    if (coords) {
+      const { latitude, longitude } = coords;
+      let response = await Location.reverseGeocodeAsync({
+        latitude,
+        longitude,
+      });
+
+      for (let item of response) {
+        let address = `${item.name}, ${item.street}, ${item.postalCode}, ${item.city}`;
+
+        setDisplayCurrentAddress(address);
+      }
+    }
+  };
+
+  return (
+    <View>
+      <Text>GeolocationHandler {displayCurrentAddress}</Text>
+    </View>
+  );
+};
+
+export default GeolocationHandler;
+
+const styles = StyleSheet.create({});
